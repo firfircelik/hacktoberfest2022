5f65c0d8e00b81467b1a47f276ef4612dd7aafa3
firfircelik <firatcelik@aol.com>
2022-10-17 13:10:56 +0300

    chore: add second part

--- analysis_sales/analysis.py
+++ analysis_sales/analysis.py
@@ -12,3 +12,10 @@ perc =[.20, .40, .60, .80]
 include =['object', 'float', 'int']
 desc = sales_data.describe(percentiles = perc, include = include)
 desc
+
+out = pd.cut(sales_data['Avg_Price_per_unit'], bins=[0, 10, 20, 40,60, 80, 100, 120], include_lowest=True)
+out_norm = out.value_counts(sort=False, normalize=True).mul(100)
+ax = out_norm.plot.bar(rot=0, color="b", figsize=(15,6))
+plt.ylabel("frequency")
+plt.xlabel("unit_price")
+plt.show()
